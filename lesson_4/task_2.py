076b8b913154612184b757c51ddcb47276bbb60d
Nicolae Culeva <culevanic@gmail.com>
2024-04-12 09:29:51 +0300

    Homework done for lesson_4

--- lesson_4/task_2.py
+++ lesson_4/task_2.py
@@ -34,91 +34,94 @@ print(name[:3])
 # Acum printează valoarea variabilei `name` în majuscule folosind metoda `upper`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name.upper())
 # CODUL TĂU VINE MAI SUS:
 
 # Acum printează valoarea variabilei `name` în minuscule folosind metoda `lower`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name.lower())
 # CODUL TĂU VINE MAI SUS:
 
 # Acum printează concatenarea variabilelor `name` și `name2`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name + name2)
 # CODUL TĂU VINE MAI SUS:
 
 # Creează o variabilă `text` și setează-i un text la alegere, cu restricția ca acesta să conțină mai multe rânduri
 
 # CODUL TĂU VINE MAI JOS:
-
+text = 'Odată cu sosirea primăverii \ntotul revine la viață. \nPomii se îmbracă în straie verzi, \nFlorile ne bucură cu culori divine, \nPăsărelele cântă vesel.'
+print(text)
 # CODUL TĂU VINE MAI SUS:
 
 # Verifică dacă variabila `text` conține cuvântul `python`
 
 # CODUL TĂU VINE MAI JOS:
-
+print('python' in text)
 # CODUL TĂU VINE MAI SUS:
 
 # Folosește metoda replace pentru a înlocui litera `a` din variabila `text` cu litera `e`
 
 # CODUL TĂU VINE MAI JOS:
-
+print (text.replace('a', 'e'))
 # CODUL TĂU VINE MAI SUS:
 
 # Folosește metoda split pentru a transforma variabila `text` într-o listă de cuvinte
 
 # CODUL TĂU VINE MAI JOS:
-
+print(text.split())
 # CODUL TĂU VINE MAI SUS:
 
 # Creează un f-string care să conțină variabilele `name` și `name2`
 
 # CODUL TĂU VINE MAI JOS:
-
+f_string = f'Eu ma numesc {name} si am un verisor cu numele {name2}!'
 # CODUL TĂU VINE MAI SUS:
 
 # Verifică dacă string-ul creat se termină cu `!`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f_string.endswith('!'))
 # CODUL TĂU VINE MAI SUS:
 
 # Verifică dacă string-ul creat începe cu `Hello`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f_string.startswith('Hello'))
 # CODUL TĂU VINE MAI SUS:
 
 # Identifică indexul unde se află `!` în string-ul creat
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f_string.find('!'))
 # CODUL TĂU VINE MAI SUS:
 
 # Identifică indexul unde se află `o` în string-ul creat folosind altă metodă
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f_string.index('o'))
 # CODUL TĂU VINE MAI SUS:
 
 # Utilizând format string-uri, creează un string care să conțină variabilele `name` și `name2`
 
 # CODUL TĂU VINE MAI JOS:
-
+format_string = 'Eu sunt {} si vecinul meu este {}.'.format(name, name2)
+print(format_string)
 # CODUL TĂU VINE MAI SUS:
 
 # Concatenează string-ul creat cu string-ul `text`
 
 # CODUL TĂU VINE MAI JOS:
-
+string1 = format_string + text
+print(string1)
 # CODUL TĂU VINE MAI SUS:
 
 # Afișează lungimea string-ului creat
 
 # CODUL TĂU VINE MAI JOS:
-
+print(len(string1))
 # CODUL TĂU VINE MAI SUS:
 
 # Aceasta a fost tot pentru această sarcină
